55027cd41a96bcf3018fef62a385df7fd555fd60
samulinas <samulinas@gmail.com>
2024-04-22 11:49:09 +0300

    Добавьте файлы проекта.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using BookShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext? _db;
+
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
+        }
+
+        // GET CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid) {
+                _db?.Category.Add(obj);
+                _db?.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookShop.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Введіть значення для поля!")]
+        [StringLength(50)]
+        [DisplayName("Назва категорії")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Введіть значення для поля!")]
+        [DisplayName("Порядок відображення")]
+        [Range(1, int.MaxValue, ErrorMessage = "Значення порядку відображення повинно бути більше нуля!")]
+        public int DisplayOrder { get; set; }
+    }
+}
